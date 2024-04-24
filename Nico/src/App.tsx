b600067e15655b6a8c1704b6a4d9180f92d90664
Emilyuwu0 <tbarrera679@gmail.com>
2024-04-24 18:48:57 -0500

    💙

--- Nico/src/App.tsx
+++ Nico/src/App.tsx
@@ -1,34 +1,89 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from "react";
+import Modal from "./Components/Modal/Modal";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [modalOpen, setModalOpen] = useState(false);
+  const handleDivClick = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+    <div onClick={handleDivClick}>
+
+    <div className="container noselect">
+  <div className="canvas">
+    <div className="tracker tr-1"></div>
+    <div className="tracker tr-2"></div>
+    <div className="tracker tr-3"></div>
+    <div className="tracker tr-4"></div>
+    <div className="tracker tr-5"></div>
+    <div className="tracker tr-6"></div>
+    <div className="tracker tr-7"></div>
+    <div className="tracker tr-8"></div>
+    <div className="tracker tr-9"></div>
+    <div className="tracker tr-10"></div>
+    <div className="tracker tr-11"></div>
+    <div className="tracker tr-12"></div>
+    <div className="tracker tr-13"></div>
+    <div className="tracker tr-14"></div>
+    <div className="tracker tr-15"></div>
+    <div className="tracker tr-16"></div>
+    <div className="tracker tr-17"></div>
+    <div className="tracker tr-18"></div>
+    <div className="tracker tr-19"></div>
+    <div className="tracker tr-20"></div>
+    <div className="tracker tr-21"></div>
+    <div className="tracker tr-22"></div>
+    <div className="tracker tr-23"></div>
+    <div className="tracker tr-24"></div>
+    <div className="tracker tr-25"></div>
+    <div id="card">
+    <div className="screen-card" onClick={ handleDivClick }>
+        <div className="screen-card-header">
+          <div className="screen-card-header-left">
+            <div className="screen-card-header-button close"></div>
+            <div className="screen-card-header-button maximize"></div>
+            <div className="screen-card-header-button minimize"></div>
+          </div>
+          <div className="screen-card-header-right">
+            
+          </div>
+        </div>
+        <div className="screen-card-body">
+          <div className="screen-card-body-item left">
+            <div className="app-title">
+              {/* <img src={img} className="width-img"/> */}
+              <span className="title-card">꒰ᐢ. .ᐢ꒱ </span>
+              <span className="app-description-card">Presioname </span>
+              <span className="app-description2-card">Pls ₊˚⊹ </span>
+            </div>
+          </div>
+       {/*    <div className="screen-card-body-item right">
+            <div className="app-title-card">
+              <span>{title2}</span>
+              <span className="app-description">{description2}</span>
+            </div>
+          </div> */}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      
+    </div>
+  </div>
+</div>
+
+    </div>
+
+
+
+    {modalOpen && <Modal onClose={closeModal} />}
+  </div>
   )
 }
 
